be1e9a822676d2309588db57a45ef30d3140915d
David Barnett <davidbarnett2@gmail.com>
2014-09-05 00:40:41 -0700

    Automatically detect implicit maktaba flags like plugin[*]

--- vimdoc/module.py
+++ vimdoc/module.py
@@ -66,34 +66,62 @@ class Module(object):
   def LookupTag(self, typ, name):
     return self.plugin.LookupTag(typ, name)
 
+  def GetCollection(self, typ):
+    """Gets a collection by type, sorting and filtering as necessary.
+
+    Applies appropriate sorting for the type. Most types are left in the same
+    order they were defined in the code since that's usually the logical order.
+    Functions are sorted by namespace but functions within a namespace are left
+    in definition order. Dicts are sorted by name.
+
+    Some blocks might be "default blocks", meaning they should be omitted if any
+    other block defines the same tag name. These will be omitted as appropriate.
+
+    Args:
+      typ: a vimdoc block type identifier (e.g., vimdoc.FUNCTION)
+    """
+    collection = self.collections.get(typ, ())
+    if typ == vimdoc.FUNCTION:
+      # Sort by namespace, but preserve order within the same namespace. This
+      # lets us avoid variability in the order files are traversed without
+      # losing all useful order information.
+      collection = sorted(collection,
+          key=lambda x: x.locals.get('namespace', ''))
+    elif typ == vimdoc.DICTIONARY:
+      collection = sorted(collection)
+    non_default_names = set(x.TagName() for x in collection
+        if not x.IsDefault())
+    return [x for x in collection
+        if not x.IsDefault() or x.TagName() not in non_default_names]
+
   def Close(self):
     """Closes the module.
 
     All default sections that have not been overridden will be created.
     """
-    if vimdoc.FUNCTION in self.collections and 'functions' not in self.sections:
+    if self.GetCollection(vimdoc.FUNCTION) and 'functions' not in self.sections:
       functions = Block()
       functions.SetType(vimdoc.SECTION)
       functions.Local(id='functions', name='Functions')
       self.Merge(functions)
-    if (vimdoc.EXCEPTION in self.collections
+    if (self.GetCollection(vimdoc.EXCEPTION)
         and 'exceptions' not in self.sections):
       exceptions = Block()
       exceptions.SetType(vimdoc.SECTION)
       exceptions.Local(id='exceptions', name='Exceptions')
       self.Merge(exceptions)
-    if vimdoc.COMMAND in self.collections and 'commands' not in self.sections:
+    if self.GetCollection(vimdoc.COMMAND) and 'commands' not in self.sections:
       commands = Block()
       commands.SetType(vimdoc.SECTION)
       commands.Local(id='commands', name='Commands')
       self.Merge(commands)
-    if vimdoc.DICTIONARY in self.collections and 'dicts' not in self.sections:
+    if self.GetCollection(vimdoc.DICTIONARY) and 'dicts' not in self.sections:
       dicts = Block()
       dicts.SetType(vimdoc.SECTION)
       dicts.Local(id='dicts', name='Dictionaries')
       self.Merge(dicts)
-    if ((vimdoc.FLAG in self.collections or
-         vimdoc.SETTING in self.collections) and
+    if ((self.GetCollection(vimdoc.FLAG) or
+         self.GetCollection(vimdoc.SETTING)) and
         'config' not in self.sections):
       config = Block()
       config.SetType(vimdoc.SECTION)
@@ -130,34 +158,25 @@ class Module(object):
     for ident, section in self.sections.items():
       yield section
       if ident == 'functions':
-        # Sort by namespace, but preserve order within the same namespace. This
-        # lets us avoid variability in the order files are traversed without
-        # losing all useful order information.
-        collection = sorted(
-            self.collections.get(vimdoc.FUNCTION, ()),
-            key=lambda x: x.locals.get('namespace', ''))
-        for block in collection:
+        for block in self.GetCollection(vimdoc.FUNCTION):
           if 'dict' not in block.locals and 'exception' not in block.locals:
             yield block
       if ident == 'commands':
-        for block in self.collections.get(vimdoc.COMMAND, ()):
+        for block in self.GetCollection(vimdoc.COMMAND):
           yield block
       if ident == 'dicts':
-        for block in sorted(self.collections.get(vimdoc.DICTIONARY, ())):
+        for block in self.GetCollection(vimdoc.DICTIONARY):
           yield block
-          collection = sorted(
-              self.collections.get(vimdoc.FUNCTION, ()),
-              key=lambda x: x.locals.get('namespace', ''))
-          for func in collection:
+          for func in self.GetCollection(vimdoc.FUNCTION):
             if func.locals.get('dict') == block.locals['dict']:
               yield func
       if ident == 'exceptions':
-        for block in self.collections.get(vimdoc.EXCEPTION, ()):
+        for block in self.GetCollection(vimdoc.EXCEPTION):
           yield block
       if ident == 'config':
-        for block in self.collections.get(vimdoc.FLAG, ()):
+        for block in self.GetCollection(vimdoc.FLAG):
           yield block
-        for block in self.collections.get(vimdoc.SETTING, ()):
+        for block in self.GetCollection(vimdoc.SETTING):
           yield block
       if ident in self.backmatters:
         yield self.backmatters[ident]
@@ -296,9 +315,30 @@ def Modules(directory):
     for f in files:
       filename = os.path.join(root, f)
       if os.path.splitext(filename)[1] == '.vim':
-        with open(filename) as filehandle:
-          blocks = list(parser.ParseBlocks(filehandle, filename))
         relative_path = os.path.relpath(filename, directory)
+        with open(filename) as filehandle:
+          lines = list(filehandle)
+          blocks = list(parser.ParseBlocks(lines, filename))
+          # Define implicit maktaba flags for files that call
+          # maktaba#plugin#Enter. These flags have to be special-cased here
+          # because there aren't necessarily associated doc comment blocks and
+          # the name is computed from the file name.
+          if (not relative_path.startswith('autoload' + os.path.sep)
+              and relative_path != os.path.join('instant', 'flags.vim')):
+            if ContainsMaktabaPluginEnterCall(lines):
+              flagpath = relative_path
+              if flagpath.startswith('after' + os.path.sep):
+                flagpath = os.path.relpath(flagpath, 'after')
+              flagblock = Block(is_default=True)
+              flagblock.SetType(vimdoc.FLAG)
+              name_parts = os.path.splitext(flagpath)[0].split(os.path.sep)
+              flagname = name_parts.pop(0)
+              flagname += ''.join('[' + p + ']' for p in name_parts)
+              flagblock.Local(name=flagname)
+              flagblock.AddLine(
+                  'Configures whether {} should be loaded.'.format(
+                      relative_path))
+              blocks.append(flagblock)
         paths_and_blocks.append((relative_path, blocks))
         if filename.startswith(autoloaddir):
           if blocks and blocks[0].globals.get('standalone'):
@@ -358,3 +398,15 @@ def GetMatchingStandalonePath(path, standalones):
     if path.startswith(os.path.splitext(standalone)[0] + os.path.sep):
       return standalone
   return None
+
+
+def ContainsMaktabaPluginEnterCall(lines):
+  """Returns whether lines of vimscript contain a maktaba#plugin#Enter call.
+
+  Args:
+    lines: A sequence of vimscript strings to search.
+  """
+  for _, line in parser.EnumerateStripNewlinesAndJoinContinuations(lines):
+    if not parser.IsComment(line) and 'maktaba#plugin#Enter(' in line:
+      return True
+  return False
