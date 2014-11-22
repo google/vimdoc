676ad42b958eff42d5605c2dd0ecbd2c88dd31a2
David Barnett <davidbarnett2@gmail.com>
2014-11-22 13:22:48 -0800

    Drop py2 support and add metadata in setup.py and stdeb.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,44 @@
 """Distribution for vimdoc."""
+import codecs
 from distutils.core import setup
 import os.path
+import sys
 
-with open(os.path.join(os.path.dirname(__file__), 'vimdoc/VERSION.txt')) as f:
+
+# Don't even try to run under python 2 or earlier. It will seem to work but fail
+# in corner cases with strange encoding errors.
+if sys.version_info[0] < 3:
+  sys.exit('ERROR: Python < 3 is unsupported.')
+
+
+VERSION_PATH = os.path.join(os.path.dirname(__file__), 'vimdoc/VERSION.txt')
+with codecs.open(VERSION_PATH, 'r', 'utf-8') as f:
   version = f.read().strip()
+
+LONG_DESCRIPTION = """
+Vimdoc is a system for automatically generating vim help files from doc
+comments embedded in vim plugin files.
+""".strip()
 
 setup(
     name='vimdoc',
     version=version,
     description='Generate vim helpfiles',
+    long_description=LONG_DESCRIPTION,
     author='Nate Soares',
     author_email='nate@so8r.es',
+    license='Apache 2.0',
     url='https://github.com/google/vimdoc',
-    packages=[
-        'vimdoc',
-    ],
+    packages=['vimdoc'],
     scripts=[
         'scripts/vimdoc',
     ],
     package_data={'vimdoc': ['VERSION.txt']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Documentation',
+    ],
 )
--- vimdoc/__init__.py
+++ vimdoc/__init__.py
@@ -1,8 +1,16 @@
 """Vimdoc: Vim helpfile generation."""
+import sys
+
+
+# Don't even try to run under python 2 or earlier. It will seem to work but fail
+# in corner cases with strange encoding errors.
+if sys.version_info[0] < 3:
+  raise ImportError('Python < 3 is unsupported')
+
 
 def __read_version_txt():
   import pkgutil
-  return pkgutil.get_data('vimdoc', 'VERSION.txt').strip()
+  return pkgutil.get_data('vimdoc', 'VERSION.txt').decode('utf-8').strip()
 
 __version__ = __read_version_txt()
 
