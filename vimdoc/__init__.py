b48cbc0138b92a7cae69f5e24209dcfdd7eef406
David Barnett <david@mumind.me>
2024-08-27 15:46:32 -0600

    Get version from setuptools-scm, remove VERSION.txt

--- vimdoc/__init__.py
+++ vimdoc/__init__.py
@@ -1,18 +1,19 @@
 """Vimdoc: Vim helpfile generation."""
 import sys
 
+try:
+  from vimdoc._version import __version__ as __version__
+except ImportError:
+  import warnings
+  warnings.warn('Failed to load __version__ from setuptools-scm')
+  __version__ = '__unknown__'
+
 
 # Don't even try to run under python 2 or earlier. It will seem to work but fail
 # in corner cases with strange encoding errors.
 if sys.version_info[0] < 3:
   raise ImportError('Python < 3 is unsupported')
 
-
-def __read_version_txt():
-  import pkgutil
-  return pkgutil.get_data('vimdoc', 'VERSION.txt').decode('utf-8').strip()
-
-__version__ = __read_version_txt()
 
 SECTION = 'SECTION'
 BACKMATTER = 'BACKMATTER'
